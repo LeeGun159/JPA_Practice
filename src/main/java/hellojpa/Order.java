e1b4d1ac8d0c4ae7be274af7d1353c7496769f26
LeeGun159 <tdyfuh7536@naver.com>
2024-12-25 00:58:05 +0900

    -상속관계 매핑

--- src/main/java/hellojpa/Order.java
+++ src/main/java/hellojpa/Order.java
@@ -12,7 +12,9 @@ public class Order {
     @GeneratedValue
     @Column(name = "ORDER_ITEM_ID")
     private Long id;
-
+    @OneToOne
+    @JoinColumn(name = "DELIVERY_ID")
+    private Delivery delivery;
     @ManyToOne
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
